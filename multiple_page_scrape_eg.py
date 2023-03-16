7097ab34828a7cc0b45545e9880b499139cbb12c
Rob <robjrenwick@gmail.com>
2023-03-16 17:27:44 +1000

    changed delay and comments added
    changed delay to 1-3 seconds and commented out the file.write code to reduce time while testing other.

--- multiple_page_scrape_eg.py
+++ multiple_page_scrape_eg.py
@@ -29,11 +29,10 @@ for link in links:
     box = soup.find('article', class_='main-article')
     title = box.find('h1').get_text()
     transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-       
-    # test print to screen instead of file
-    print(transcript)
-    # with open(f'{title}.txt', 'w') as file:
-    #     file.write(transcript)
 
-    # random 1 to 5 second delay
-    sleep(randint(1,5))
+    # commented out below to stop writing files while testing
+    #
+    # with open(f'{title}.txt', 'w') as file:
+    #     file.write(transcript)  
+    #   
+    sleep(randint(1,3))  # random 1 to 3 second delay

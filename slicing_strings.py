cedc79665d169c900de926009a8274a25aa9996e
Rob <robjrenwick@gmail.com>
2023-03-24 17:45:15 +1000

    string slicing
    concatenation and slicing strings. Also using 'in' as logical operator

---
+++ slicing_strings.py
@@ -0,0 +1,17 @@
+pc = 'laptop computer'
+print(pc[:2])  # slice start of index through to stop before index 2, so 0 & 1
+
+# concatenation
+a = 'micro'
+b = a + 'wave'  # overloading the + operator
+print(b)  # microwave
+
+c = a + ' ' + 'wave'
+print(c)  # micro wave
+
+# Using 'in' as a logical operator
+x = 'toyota'
+check = 'm' in x  # is the letter 'm' in toyota, True or False?
+print(check)  # False
+
+
